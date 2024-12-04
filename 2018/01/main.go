7d81aff93f720883a47ef7705a21080a32d4b3e4
benfrisbie <bbfrisbi@gmail.com>
2024-12-04 09:57:12 -0500

    add tests to 2018 day1

--- 2018/01/main.go
+++ 2018/01/main.go
@@ -9,20 +9,17 @@ import (
 
 func main() {
 	// Read input
-	data, err := ReadInput("input.txt")
-	if err != nil {
-		panic(err)
-	}
+	data := ReadInput("input.txt")
 
 	// Part 1
-	fmt.Println("Part 1: final frequency = ", CalculateFinalFrequency(data))
+	fmt.Println("Part 1: final frequency = ", Part1(data))
 
 	// Part 2
-	fmt.Println("Part 2: first repeating frequency = ", FindFirstRepeatingFrequency(data))
+	fmt.Println("Part 2: first repeating frequency = ", Part2(data))
 }
 
 // CalculateFinalFrequency calculates the final frequency after all the deltas are applied
-func CalculateFinalFrequency(data []int) int {
+func Part1(data []int) int {
 	freq := 0
 	for _, delta := range data {
 		freq += delta
@@ -30,8 +27,8 @@ func CalculateFinalFrequency(data []int) int {
 	return freq
 }
 
-// FindFirstRepeatingFrequency finds the first frequency that appears twice
-func FindFirstRepeatingFrequency(data []int) int {
+// Part2 finds the first frequency that appears twice
+func Part2(data []int) int {
 	found := make(map[int]bool)
 	freq := 0
 	for i := 0; ; i++ {
@@ -44,21 +41,21 @@ func FindFirstRepeatingFrequency(data []int) int {
 }
 
 // ReadInput reads the input file into a slice of ints
-func ReadInput(inputPath string) ([]int, error) {
+func ReadInput(inputPath string) []int {
 	// Loop through file and convert each line to an int
 	var data []int
 	file, err := os.Open(inputPath)
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		i, err := strconv.Atoi(scanner.Text())
 		if err != nil {
-			return nil, err
+			panic(err)
 		}
 		data = append(data, i)
 	}
-	return data, nil
+	return data
 }
